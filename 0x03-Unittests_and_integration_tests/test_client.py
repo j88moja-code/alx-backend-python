8586a864ee1b8a4eb5ce8dd8441112adbfa3b2e2
j88moja-code <sitholent+github@gmail.com>
2023-04-24 08:57:59 +0000

    4. Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,26 +1,94 @@
 #!/usr/bin/env python3
-""" Unittest module """
-
-from unittest import TestCase, mock
-from unittest.mock import patch, Mock
-from parameterized import parameterized
-
-import client
+"""Test Client File"""
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
+from unittest.mock import patch, Mock, PropertyMock
+import unittest
 
 
-class TestGithubOrgClient(TestCase):
-    """ Class for testing GithubOrgClient """
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    --------------------------
+    CLASS: TestGitHubOrgClient
+    --------------------------
+    Description:
+        A collection of unit tests testing
+        the individual functions in the
+        clients module(file)
+    """
 
     @parameterized.expand([
-        ("google"),
-        ("abc"),
+        ('google'),
+        ('abc')
     ])
     @patch('client.get_json')
-    def test_org(self, org_name, mock_json):
-        """ Test method returns correct output """
-        gc = GithubOrgClient(org_name)
-        gc.org()
-        mock_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-            )
+    def test_org(self, org_name, mocked_method):
+        """ Tests whether get_json is behaving expectedly """
+        mocked_method.return_value = {org_name: True}
+        gh = GithubOrgClient(org_name)
+
+        self.assertEqual(gh.org, {org_name: True})
+        mocked_method.assert_called_once()
+
+    @patch('client.GithubOrgClient._public_repos_url',
+           new_callable=PropertyMock)
+    def test_public_repos_url(self, mocked_method):
+        """
+        -----------------------------
+        METHOD: test_public_repos_url
+        -----------------------------
+        Tests whether the _public_repos_url is
+        correctly set and that the memoization
+        is also working accordingly.
+        """
+        expected_payload = {'gh_url': 'example.com'}
+        mocked_method.return_value = expected_payload
+
+        gh = GithubOrgClient('google')
+        url = gh._public_repos_url
+
+        self.assertEqual(url, expected_payload)
+        mocked_method.assert_called_once()
+
+    @patch('client.get_json')
+    def test_public_repos(self, mocked_get_json):
+        """
+        -------------------------
+        METHOD: test_public_repos
+        -------------------------
+        Description:
+            Tests public repos
+        """
+        mocked_get_json.return_value = {'get_json':
+                                        'some data the API returned'}
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mocked_public_repo_urls:
+            expected_payload = [{'gh_url': 'example.com'}]
+            mocked_public_repo_urls.return_value = expected_payload
+
+            gh = GithubOrgClient('google')
+            url = gh._public_repos_url
+
+            self.assertEqual(url, expected_payload)
+            mocked_public_repo_urls.assert_called_once()
+            mocked_get_json()
+            mocked_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license"),
+        ({"license": {"key": "other_license"}}, "my_license")
+    ])
+    def test_has_license(self, license, exp_license_key):
+        """
+        ------------------------
+        METHOD: test_has_license
+        ------------------------
+        Description:
+            Checks whether a license_key is present
+            in a license object.
+        """
+        self.assertEqual(GithubOrgClient.has_license(license, exp_license_key),
+                         license['license']['key'] == exp_license_key)
+if __name__ == '__main__':
+    unittest.main()
